183072e6df73173a1e410d5d0cd0301a7ce69eac
Mehmet Sami <karadumanmehmetsami27@gmail.com>
2026-04-05 14:27:12 +0300

    Kitchen Object Parent

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,16 +1,54 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KitchenObjectsSO kitchenObjectsSO;
     [SerializeField] private Transform counterTopPoint;
-   public void Interact()
-    {
-        Debug.Log("Interact!");
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectsSO.prefabs, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectsSO());
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Keyboard.current.tKey.isPressed)
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }   
     }
+    public void Interact()
+   {
+        if (kitchenObjectsSO != null)
+        {
+            
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectsSO.prefabs, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+            
+        }else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+   }
+
+
+   public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+   }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject; 
+    }
+
+    public KitchenObject GetKitchenObject() { return kitchenObject; }
+
+    public void ClearKitchenObject() { kitchenObject = null; }
+
+    public bool HasKitchenObject() { return kitchenObject  != null; }
+
 }
